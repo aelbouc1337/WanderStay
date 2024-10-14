da579286b8d6dbdc3bda66b9207159fa97ac44db
Abderrahman el bouchikhi <83993363+aelbouc1337@users.noreply.github.com>
2024-10-14 20:11:34 +0100

    backend Completed

--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import authRouter from "./routes/auth.js";
-import roomsRouter from "./routes/rooms.js";
-import hotelsRouter from "./routes/hotels.js";
-import usersRouter from "./routes/users.js";
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
@@ -12,26 +14,38 @@ dotenv.config();
 const connect = async () => {
   try {
     await mongoose.connect(process.env.DB_STRING);
-    console.log("Connected to MongoDB.");
+    console.log("Connected to mongoDB.");
   } catch (error) {
     throw error;
   }
 };
 
 mongoose.connection.on("disconnected", () => {
-  console.log("MongoDB Disconnected");
-});
-mongoose.connection.on("connected", () => {
-  console.log("MongoDB Connected");
+  console.log("mongoDB disconnected!");
 });
 
-// Midllewares
-app.use("/api/auth", authRouter);
-app.use("/api/rooms", roomsRouter);
-app.use("/api/hotels", hotelsRouter);
-app.use("/api/users", usersRouter);
+//middlewares
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
 
-app.listen(8000, () => {
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
+
+app.listen(8800, () => {
   connect();
-  console.log("server running ....");
+  console.log("Connected to backend.");
 });
